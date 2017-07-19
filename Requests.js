11cba634f1701adbb289a6726c8ab4d486bf896b
Baroquem <mjc12@cornell.edu>
2017-07-19 12:58:20 -0400

    Add search box

--- Requests.js
+++ Requests.js
@@ -4,10 +4,14 @@ import Route from 'react-router-dom/Route';
 import queryString from 'query-string';
 
 import FilterGroups, { initialFilterState, onChangeFilter as commonChangeFilter } from '@folio/stripes-components/lib/FilterGroups';
+import FilterPaneSearch from '@folio/stripes-components/lib/FilterPaneSearch';
 import Pane from '@folio/stripes-components/lib/Pane';
 import Paneset from '@folio/stripes-components/lib/Paneset';
 import PaneMenu from '@folio/stripes-components/lib/PaneMenu';
+import transitionToParams from '@folio/stripes-components/util/transitionToParams';
 
+const INITIAL_RESULT_COUNT = 30;
+const RESULT_COUNT_INCREMENT = 30;
 
 const filterConfig = [
   {
@@ -32,6 +36,11 @@ class Requests extends React.Component {
     match: PropTypes.shape({
       path: PropTypes.string.isRequired,
     }).isRequired,
+    mutator: PropTypes.shape({
+      requestCount: PropTypes.shape({
+        replace: PropTypes.func,
+      }),
+    }).isRequired,
     okapi: PropTypes.object,
     stripes: PropTypes.shape({
       connect: PropTypes.func.isRequired,
@@ -39,6 +48,10 @@ class Requests extends React.Component {
         log: PropTypes.func.isRequired,
       }).isRequired,
     }).isRequired,
+  };
+
+  static manifest = {
+    requestCount: { initialValue: INITIAL_RESULT_COUNT },
   };
 
   constructor(props) {
@@ -53,11 +66,32 @@ class Requests extends React.Component {
     };
 
     this.onChangeFilter = commonChangeFilter.bind(this);
-    this.updateFilters = updateFilters.bind(this);
-
+    this.onChangeSearch = this.onChangeSearch.bind(this);
+    this.onClearSearch = this.onClearSearch.bind(this);
+    this.transitionToParams = transitionToParams.bind(this);
+    this.updateFilters = this.updateFilters.bind(this);
   }
 
+  /* ************** Search handlers ************** */
+  onChangeSearch = (e) => {
+    this.props.mutator.requestCount.replace(INITIAL_RESULT_COUNT);
+    const query = e.target.value;
+    this.setState({ searchTerm: query });
+    this.performSearch(query);
+  }
+
+  onClearSearch = () => {
+    this.setState({ searchTerm: '' });
+    this.props.history.push(this.props.location.pathname);
+  }
+
+  performSearch = _.debounce((query) => {
+    this.transitionToParams({ query });
+  }, 250);
+
+  /* ************** Filter handlers ************** */
   onChangeFilter = (e) => {
+    this.props.mutator.requestCount.replace(INITIAL_RESULT_COUNT);
     this.commonChangeFilter(e);
   }
 
@@ -67,9 +101,18 @@ class Requests extends React.Component {
   }
 
   render() {
+
+    const searchHeader = <FilterPaneSearch
+      id="SearchField"
+      onChange={this.onChangeSearch}
+      onClear={this.onClearSearch}
+      value={this.state.searchTerm}
+      searchAriaLabel="Requests search"
+    />;
+
     return (
       <Paneset>
-        <Pane defaultWidth="16%" header="{searchHeader}">
+        <Pane defaultWidth="16%" header={searchHeader}>
           <FilterGroups config={filterConfig} filters={this.state.filters} onChangeFilter={this.onChangeFilter} />
         </Pane>
         <Pane defaultWidth="fill" paneTitle="Requests">
