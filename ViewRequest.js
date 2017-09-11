0312bb2e7495fb5af8c5f5282d42eedde4a1517c
Matt Connolly <mjc12@cornell.edu>
2017-09-11 10:19:07 -0400

    Add links to item and user records

--- ViewRequest.js
+++ ViewRequest.js
@@ -2,6 +2,7 @@ import _ from 'lodash';
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Row, Col } from 'react-flexbox-grid';
+import { Link } from 'react-router-dom';
 
 import KeyValue from '@folio/stripes-components/lib/KeyValue';
 import Pane from '@folio/stripes-components/lib/Pane';
@@ -69,6 +70,10 @@ class ViewRequest extends React.Component {
     let request = (resources.selectedRequest && resources.selectedRequest.hasLoaded) ? resources.selectedRequest.records[0] : null;
     
     let patronGroup, borrower, borrowerName, borrowerGroup;
+    
+    // Most of the values needed to populate the view come from the "enhanced" request
+    // object, which includes parts of the requester's user record, the item record,
+    // and the related loan record (if any) and its borrower.
     if (this.state.enhancedRequest.id) {
       request = this.state.enhancedRequest;
       patronGroup = request.patronGroup;
@@ -80,6 +85,15 @@ class ViewRequest extends React.Component {
         borrowerGroup = resources.patronGroups.records.find(g => g.id === borrowerGroup).group || borrowerGroup;
       }
     }
+    
+    let itemRecordLink, requesterRecordLink;
+    const itemBarcode = _.get(request, ['itemBarcode'], '');
+    itemRecordLink = itemBarcode ? <Link to={`/items/view/${request.itemId}`}>{itemBarcode}</Link> : '';
+    
+    const requesterName = _.get(request, ['requesterName'], '');
+    requesterRecordLink = requesterName ? <Link to={`/users/view/${request.requesterId}`}>{requesterName}</Link> : '';
+    
+    const borrowerRecordLink = borrowerName ? <Link to={`/users/view/${borrower.id}`}>{borrowerName}</Link> : '';
 
     return request ? (
       <Pane defaultWidth={this.props.paneWidth} paneTitle="Request Detail" dismissible onClose={this.props.onClose}>
@@ -92,7 +106,7 @@ class ViewRequest extends React.Component {
           <legend>Item info</legend>
           <Row>
             <Col xs={12}>
-              <KeyValue label="Item barcode" value={_.get(request, ['itemBarcode'], '')} />
+              <KeyValue label="Item barcode" value={itemRecordLink} />
             </Col>
           </Row>
           <Row>
@@ -115,7 +129,7 @@ class ViewRequest extends React.Component {
           <legend>Current Loan</legend>
           <Row>
             <Col xs={4}>
-              <KeyValue label="Loaned to" value={borrowerName} />
+              <KeyValue label="Loaned to" value={borrowerRecordLink} />
             </Col>
             <Col xs={4}>
               <KeyValue label="Patron group" value={borrowerGroup} />
@@ -137,7 +151,7 @@ class ViewRequest extends React.Component {
           <legend>Requester info</legend>
           <Row>
             <Col xs={12}>
-              <KeyValue label="Requester name" value={_.get(request, ['requesterName'], '')} />
+              <KeyValue label="Requester name" value={requesterRecordLink} />
             </Col>
           </Row>
           <Row>
