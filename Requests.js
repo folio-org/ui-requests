263cc6d5ae1a7b0bd48b348dbe187ee51c1a4389
Matt Connolly <mjc12@cornell.edu>
2018-02-07 15:44:11 -0500

    Remove code made redundant by <SearchAndSort>. Hooray!

--- Requests.js
+++ Requests.js
@@ -1,25 +1,10 @@
-import _ from 'lodash';
 import React from 'react';
 import PropTypes from 'prop-types';
-import Route from 'react-router-dom/Route';
-import queryString from 'query-string';
 import fetch from 'isomorphic-fetch';
 import { FormattedDate } from 'react-intl';
 
-import Button from '@folio/stripes-components/lib/Button';
-import { filters2cql, initialFilterState, onChangeFilter as commonChangeFilter, handleFilterClear, handleClearAllFilters } from '@folio/stripes-components/lib/FilterGroups';
-import FilterPaneSearch from '@folio/stripes-components/lib/FilterPaneSearch';
-import Layer from '@folio/stripes-components/lib/Layer';
-import MultiColumnList from '@folio/stripes-components/lib/MultiColumnList';
-import Notes from '@folio/stripes-smart-components/lib/Notes';
-import Pane from '@folio/stripes-components/lib/Pane';
-import Paneset from '@folio/stripes-components/lib/Paneset';
-import PaneMenu from '@folio/stripes-components/lib/PaneMenu';
+import { filters2cql } from '@folio/stripes-components/lib/FilterGroups';
 import SearchAndSort from '@folio/stripes-smart-components/lib/SearchAndSort';
-import makeQueryFunction from '@folio/stripes-components/util/makeQueryFunction';
-import transitionToParams from '@folio/stripes-components/util/transitionToParams';
-import removeQueryParam from '@folio/stripes-components/util/removeQueryParam';
-import craftLayerUrl from '@folio/stripes-components/util/craftLayerUrl';
 
 import ViewRequest from './ViewRequest';
 import RequestForm from './RequestForm';
@@ -64,11 +49,7 @@ class Requests extends React.Component {
       push: PropTypes.func.isRequired,
     }).isRequired,
     location: PropTypes.shape({
-      pathname: PropTypes.string.isRequired,
       search: PropTypes.string,
-    }).isRequired,
-    match: PropTypes.shape({
-      path: PropTypes.string.isRequired,
     }).isRequired,
     mutator: PropTypes.shape({
       requests: PropTypes.shape({
@@ -79,7 +60,6 @@ class Requests extends React.Component {
         replace: PropTypes.func,
       }),
     }).isRequired,
-    notes: PropTypes.arrayOf(PropTypes.object),
     resources: PropTypes.shape({
       addressTypes: PropTypes.shape({
         hasLoaded: PropTypes.bool.isRequired,
@@ -100,10 +80,6 @@ class Requests extends React.Component {
         log: PropTypes.func.isRequired,
       }).isRequired,
     }).isRequired,
-  };
-
-  static defaultProps = {
-    notes: [],
   };
 
   static manifest = {
@@ -179,17 +155,6 @@ class Requests extends React.Component {
         staticFallback: { params: {} },
       },
     },
-    notes: {
-      type: 'okapi',
-      path: 'notes',
-      records: 'notes',
-      clear: false,
-      GET: {
-        params: {
-          query: 'link=:{id}',
-        },
-      },
-    },
     patronGroups: {
       type: 'okapi',
       path: 'groups',
@@ -221,123 +186,12 @@ class Requests extends React.Component {
       'Content-Type': 'application/json',
     });
 
-    const query = props.location.search ? queryString.parse(props.location.search) : {};
-    this.state = {
-      filters: initialFilterState(filterConfig, query.filters),
-      selectedItem: {},
-      searchTerm: query.query || '',
-      showNotesPane: false,
-      sortOrder: query.sort || '',
-    };
-
     this.addRequestFields = this.addRequestFields.bind(this);
-    this.collapseDetails = this.collapseDetails.bind(this);
-    this.connectedNotes = props.stripes.connect(Notes);
-    this.connectedViewRequest = props.stripes.connect(ViewRequest);
     this.create = this.create.bind(this);
     this.findItem = this.findItem.bind(this);
     this.findLoan = this.findLoan.bind(this);
     this.findRequestsForItem = this.findRequestsForItem.bind(this);
     this.findUser = this.findUser.bind(this);
-    this.onChangeFilter = commonChangeFilter.bind(this);
-    this.handleFilterClear = handleFilterClear.bind(this);
-    this.handleClearAllFilters = handleClearAllFilters.bind(this);
-    this.onChangeSearch = this.onChangeSearch.bind(this);
-    this.onClearSearch = this.onClearSearch.bind(this);
-    this.onClickAddNewRequest = this.onClickAddNewRequest.bind(this);
-    this.onClickCloseNewRequest = this.onClickCloseNewRequest.bind(this);
-    this.onSelectRow = this.onSelectRow.bind(this);
-    this.onSort = this.onSort.bind(this);
-    this.toggleNotes = this.toggleNotes.bind(this);
-    this.transitionToParams = transitionToParams.bind(this);
-    this.removeQueryParam = removeQueryParam.bind(this);
-    this.craftLayerUrl = craftLayerUrl.bind(this);
-    this.updateFilters = this.updateFilters.bind(this);
-  }
-
-  onSort(e, meta) {
-    const newOrder = meta.alias;
-    const oldOrder = this.state.sortOrder;
-
-    const orders = oldOrder ? oldOrder.split(',') : [];
-    if (orders.length > 0 && newOrder === orders[0].replace(/^-/, '')) {
-      orders[0] = `-${orders[0]}`.replace(/^--/, '');
-    } else {
-      orders.unshift(newOrder);
-    }
-
-    const sortOrder = orders.slice(0, 2).join(',');
-    this.setState({ sortOrder });
-    this.transitionToParams({ sort: sortOrder });
-  }
-
-  /* ************** Search handlers ************** */
-  onChangeSearch = (e) => {
-    this.props.mutator.requestCount.replace(INITIAL_RESULT_COUNT);
-    const query = e.target.value;
-    this.setState({ searchTerm: query });
-    this.performSearch(query);
-  }
-
-  onClearSearch = () => {
-    this.setState({ searchTerm: '' });
-    this.props.history.push(this.props.location.pathname);
-  }
-
-  /* ************** Filter handlers ************** */
-  onChangeFilter = (e) => {
-    this.props.mutator.requestCount.replace(INITIAL_RESULT_COUNT);
-    this.commonChangeFilter(e);
-  }
-
-  onClearFilter = (e) => {
-    this.handleFilterClear(e);
-  }
-
-  onClearAllFilters = () => {
-    this.handleClearAllFilters();
-  }
-
-
-  onSelectRow(e, meta) {
-    const requestId = meta.id;
-    this.setState({ selectedItem: meta });
-    this.props.history.push(`/requests/view/${requestId}${this.props.location.search}`);
-  }
-
-  onClickAddNewRequest(e) {
-    if (e) e.preventDefault();
-    this.transitionToParams({ layer: 'create' });
-  }
-
-  onClickCloseNewRequest(e) {
-    if (e) e.preventDefault();
-    this.removeQueryParam('layer');
-  }
-
-  performSearch = _.debounce((query) => {
-    this.transitionToParams({ query });
-  }, 250);
-
-  // provided for onChangeFilter
-  updateFilters(filters) {
-    this.transitionToParams({ filters: Object.keys(filters).filter(key => filters[key]).join(',') });
-  }
-
-  collapseDetails() {
-    this.setState({
-      selectedItem: {},
-    });
-    this.props.history.push(`${this.props.match.path}${this.props.location.search}`);
-  }
-
-  toggleNotes() {
-    this.setState((curState) => {
-      const show = !curState.showNotesPane;
-      return {
-        showNotesPane: show,
-      };
-    });
   }
 
   // idType can be 'id', 'barcode', etc.
@@ -425,38 +279,9 @@ class Requests extends React.Component {
   };
 
   render() {
-    const { stripes, resources, location } = this.props;
-    const query = location.search ? queryString.parse(location.search) : {};
-    const requests = (resources.requests || {}).records || [];
+    const { resources } = this.props;
     const patronGroups = resources.patronGroups;// (resources.patronGroups || {}).records || [];
-    const addressTypes = (this.props.resources.addressTypes && this.props.resources.addressTypes.hasLoaded) ? this.props.resources.addressTypes.records : [];
-
-    // NOTE: Uncommenting this clause will activate front-end joins of
-    // user and item records for every request in the results list. This is
-    // probably NOT something we want to do. It's here in case we need something
-    // like this later for some reason, but it shouldn't be used under
-    // normal circumstances -- and should be removed entirely at some point.
-    if (requests.length > 0) {
-      // requests = requests.map(this.addRequestFields);
-    }
-
-    const searchHeader = (<FilterPaneSearch
-      id="SearchField"
-      onChange={this.onChangeSearch}
-      onClear={this.onClearSearch}
-      value={this.state.searchTerm}
-      searchAriaLabel="Requests search"
-    />);
-
-    const paneTitle = (
-      <div style={{ textAlign: 'center' }}>
-        <strong>Results</strong>
-        <div>
-          <em>{requests && requests.length > 0 ? requests.length : '0'} Result{requests.length === 1 ? '' : 's'} Found
-          </em>
-        </div>
-      </div>
-    );
+    const addressTypes = (resources.addressTypes && resources.addressTypes.hasLoaded) ? resources.addressTypes.records : [];
 
     const resultsFormatter = {
       'Item Barcode': rq => (rq.item ? rq.item.barcode : ''),
@@ -465,10 +290,6 @@ class Requests extends React.Component {
       'Requester Barcode': rq => (rq.requester ? rq.requester.barcode : ''),
       'Request Type': rq => rq.requestType,
       Title: rq => (rq.item ? rq.item.title : ''),
-    };
-
-    const columnMapping = {
-      Author: 'author',
     };
 
     return (<SearchAndSort
