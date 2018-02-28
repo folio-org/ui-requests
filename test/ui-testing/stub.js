cd09043cec8fe64890969da1a25448da7d635fa8
Charles Ledvina <charles@indexdata.com>
2018-02-28 15:00:03 -0600

    Fix lint errors.

--- test/ui-testing/stub.js
+++ test/ui-testing/stub.js
@@ -1,24 +1,24 @@
-module.exports.test = function(uiTestCtx) {
+/* global describe it before after Nightmare */
 
-  describe('Module test: checkout:stub', function() {
+module.exports.test = function uiTest(uiTestCtx) {
+  describe('Module test: checkout:stub', function modTest() {
     const { config, helpers: { login, openApp, logout }, meta: { testVersion } } = uiTestCtx;
     const nightmare = new Nightmare(config.nightmare);
 
     this.timeout(Number(config.test_timeout));
 
     describe('Login > Open module "Requests" > Logout', () => {
-      before( done => {
-        login(nightmare, config, done);  // logs in with the default admin credentials
-      })
-      after( done => {
+      before((done) => {
+        login(nightmare, config, done); // logs in with the default admin credentials
+      });
+      after((done) => {
         logout(nightmare, config, done);
-      })
-      it('should open module "Requests" and find version tag ', done => {
+      });
+      it('should open module "Requests" and find version tag ', (done) => {
         nightmare
-        .use(openApp(nightmare, config, done, 'requests', testVersion))
-        .then(result => result )
-      })
-    })
-  })
-}
-
+          .use(openApp(nightmare, config, done, 'requests', testVersion))
+          .then(result => result);
+      });
+    });
+  });
+};
