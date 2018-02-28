650f15d69870b7ceaee8b92ec868dcfe3a5a028b
Charles Ledvina <charles@indexdata.com>
2018-02-28 16:12:31 -0600

    Added barebones filters test.

---
+++ test/ui-testing/filters.js
@@ -0,0 +1,29 @@
+/* global describe it before after Nightmare */
+
+module.exports.test = function uiTest(uiTestCtx) {
+  describe('Module test: requests:filters', function modTest() {
+    const { config, helpers: { login, openApp, logout }, meta: { testVersion } } = uiTestCtx;
+    const nightmare = new Nightmare(config.nightmare);
+
+    this.timeout(Number(config.test_timeout));
+
+    describe('Login > Open module "Requests" > Get hit counts > Click filters > Logout', () => {
+      before((done) => {
+        login(nightmare, config, done); // logs in with the default admin credentials
+      });
+      after((done) => {
+        logout(nightmare, config, done);
+      });
+      it('should open module "Requests" and find version tag ', (done) => {
+        nightmare
+          .use(openApp(nightmare, config, done, 'requests', testVersion))
+          .then(result => result);
+      });
+      it('should find hit count with no filters applied ', (done) => {
+        nightmare
+	  .wait('p[title*="Records found"]')
+          .then(done);
+      });
+    });
+  });
+};
