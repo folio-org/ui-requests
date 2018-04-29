b3432f4021218a9de42203deb9c48ef2ab836684
Matt Connolly <mjc12@cornell.edu>
2018-04-29 16:40:35 -0400

    Convert UserDetail to class

--- UserDetail.js
+++ UserDetail.js
@@ -9,118 +9,122 @@ import Select from '@folio/stripes-components/lib/Select';
 
 import { getFullName, userHighlightBox } from './utils';
 
-const UserDetail = ({
-  user,
-  requestMeta,
-  newUser,
-  patronGroup,
-  deliveryAddress,
-  deliveryLocations,
-  pickupLocation,
-  selectedDelivery,
-  fulfilmentTypeOptions,
-  onChangeAddress,
-  onChangeFulfilment,
-}) => {
-  const id = user.id;
-  const name = getFullName(user);
-  const barcode = user.barcode;
-  const proxy = _.get(requestMeta, ['proxy']);
-  let proxyName;
-  let proxyBarcode;
-  if (proxy) {
-    proxyName = getFullName(proxy);
-    proxyBarcode = _.get(proxy, ['barcode'], '-');
-  }
-  const proxySection = proxy ? userHighlightBox('Requester\'s proxy', proxyName, proxy.id, proxyBarcode) : '';
+class UserDetail extends React.Component {
 
-  return (
-    <div>
-      {userHighlightBox('Requester', name, id, barcode)}
-      <Row>
-        <Col xs={4}>
-          <KeyValue label="Patron group" value={patronGroup || '-'} />
-        </Col>
-        <Col xs={4}>
-          { newUser &&
-            <Field
-              name="fulfilmentPreference"
-              label="Fulfilment preference"
-              component={Select}
-              fullWidth
-              dataOptions={fulfilmentTypeOptions}
-              onChange={onChangeFulfilment}
-            />
-          }
-          { !newUser &&
-            <KeyValue label="Fulfilment preference" value={_.get(requestMeta, ['fulfilmentPreference'], '-')} />
-          }
-        </Col>
-        <Col xs={4}>
-          { newUser && selectedDelivery && deliveryLocations &&
-            <Field
-              name="deliveryAddressTypeId"
-              label="Delivery address"
-              component={Select}
-              fullWidth
-              dataOptions={[{ label: 'Select address type', value: '' }, ...deliveryLocations]}
-              onChange={onChangeAddress}
-            />
-          }
-          { newUser && !selectedDelivery &&
-            <Field
-              name="pickupLocationId"
-              label="Pickup location"
-              component={Select}
-              fullWidth
-              dataOptions={[{ label: 'Select pickup location', value: '' }]}
-              onChange={onChangeAddress}
-            />
-          }
-          { !newUser && selectedDelivery &&
-            <KeyValue label="Delivery address" value={deliveryAddress || '-'} />
-          }
-          { !newUser && !selectedDelivery &&
-            <KeyValue label="Pickup location" value={pickupLocation || '-'} />
-          }
-        </Col>
-      </Row>
-      { newUser && selectedDelivery &&
+  static propTypes = {
+    deliveryAddress: PropTypes.string,
+    deliveryLocations: PropTypes.arrayOf(PropTypes.object),
+    fulfilmentTypeOptions: PropTypes.arrayOf(PropTypes.object),
+    newUser: PropTypes.bool,
+    onChangeAddress: PropTypes.func,
+    onChangeFulfilment: PropTypes.func,
+    patronGroup: PropTypes.string,
+    pickupLocation: PropTypes.string,
+    requestMeta: PropTypes.object.isRequired,
+    user: PropTypes.object.isRequired,
+    selectedDelivery: PropTypes.bool,
+  };
+
+  static  defaultProps = {
+    deliveryAddress: '',
+    deliveryLocations: [],
+    fulfilmentTypeOptions: [],
+    newUser: false,
+    onChangeAddress: () => {},
+    onChangeFulfilment: () => {},
+    patronGroup: '',
+    pickupLocation: '',
+    selectedDelivery: false,
+  };
+
+  render() {
+
+    const { user,
+    requestMeta,
+    newUser,
+    patronGroup,
+    deliveryAddress,
+    deliveryLocations,
+    pickupLocation,
+    selectedDelivery,
+    fulfilmentTypeOptions,
+    onChangeAddress,
+    onChangeFulfilment } = this.props;
+
+    const id = user.id;
+    const name = getFullName(user);
+    const barcode = user.barcode;
+    const proxy = _.get(requestMeta, ['proxy']);
+    let proxyName;
+    let proxyBarcode;
+    if (proxy) {
+      proxyName = getFullName(proxy);
+      proxyBarcode = _.get(proxy, ['barcode'], '-');
+    }
+    const proxySection = proxy ? userHighlightBox('Requester\'s proxy', proxyName, proxy.id, proxyBarcode) : '';
+
+    return (
+      <div>
+        {userHighlightBox('Requester', name, id, barcode)}
         <Row>
-          <Col xs={4} xsOffset={8}>
-            {deliveryAddress}
+          <Col xs={4}>
+            <KeyValue label="Patron group" value={patronGroup || '-'} />
+          </Col>
+          <Col xs={4}>
+            { newUser &&
+              <Field
+                name="fulfilmentPreference"
+                label="Fulfilment preference"
+                component={Select}
+                fullWidth
+                dataOptions={fulfilmentTypeOptions}
+                onChange={onChangeFulfilment}
+              />
+            }
+            { !newUser &&
+              <KeyValue label="Fulfilment preference" value={_.get(requestMeta, ['fulfilmentPreference'], '-')} />
+            }
+          </Col>
+          <Col xs={4}>
+            { newUser && selectedDelivery && deliveryLocations &&
+              <Field
+                name="deliveryAddressTypeId"
+                label="Delivery address"
+                component={Select}
+                fullWidth
+                dataOptions={[{ label: 'Select address type', value: '' }, ...deliveryLocations]}
+                onChange={onChangeAddress}
+              />
+            }
+            { newUser && !selectedDelivery &&
+              <Field
+                name="pickupLocationId"
+                label="Pickup location"
+                component={Select}
+                fullWidth
+                dataOptions={[{ label: 'Select pickup location', value: '' }]}
+                onChange={onChangeAddress}
+              />
+            }
+            { !newUser && selectedDelivery &&
+              <KeyValue label="Delivery address" value={deliveryAddress || '-'} />
+            }
+            { !newUser && !selectedDelivery &&
+              <KeyValue label="Pickup location" value={pickupLocation || '-'} />
+            }
           </Col>
         </Row>
-      }
-      {proxySection}
-    </div>
-  );
-};
-
-UserDetail.propTypes = {
-  deliveryAddress: PropTypes.string,
-  deliveryLocations: PropTypes.arrayOf(PropTypes.object),
-  fulfilmentTypeOptions: PropTypes.arrayOf(PropTypes.object),
-  newUser: PropTypes.bool,
-  onChangeAddress: PropTypes.func,
-  onChangeFulfilment: PropTypes.func,
-  patronGroup: PropTypes.string,
-  pickupLocation: PropTypes.string,
-  requestMeta: PropTypes.object.isRequired,
-  user: PropTypes.object.isRequired,
-  selectedDelivery: PropTypes.bool,
-};
-
-UserDetail.defaultProps = {
-  deliveryAddress: '',
-  deliveryLocations: [],
-  fulfilmentTypeOptions: [],
-  newUser: false,
-  onChangeAddress: () => {},
-  onChangeFulfilment: () => {},
-  patronGroup: '',
-  pickupLocation: '',
-  selectedDelivery: false,
-};
+        { newUser && selectedDelivery &&
+          <Row>
+            <Col xs={4} xsOffset={8}>
+              {deliveryAddress}
+            </Col>
+          </Row>
+        }
+        {proxySection}
+      </div>
+    );
+  }
+}
 
 export default UserDetail;
