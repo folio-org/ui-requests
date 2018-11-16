ada7ac5cb034362d0b689ac3a2d6d513fa716ad1
ADITYA MATUKUMALLI <aditya.matukumalli@gmail.com>
2018-11-16 15:35:48 -0500

    Revert "Uireq 114 export csv (#166)"
    This reverts commit 5758c60dbb6573bb9968044ff0467db3f5ca849f.

--- src/Requests.js
+++ src/Requests.js
@@ -169,8 +169,6 @@ class Requests extends React.Component {
     this.addRequestFields = this.addRequestFields.bind(this);
     this.create = this.create.bind(this);
     this.findResource = this.findResource.bind(this);
-    this.headersMapping = this.headersMapping.bind(this);
-    this.buildRecords = this.buildRecords.bind(this);
   }
 
   componentDidUpdate() {
@@ -178,44 +176,13 @@ class Requests extends React.Component {
       const recordsLoaded = this.props.resources.records.records;
       const numTotalRecords = this.props.resources.records.other.totalRecords;
       if (recordsLoaded.length === numTotalRecords) {
-        const recordsToCSV = this.buildRecords(recordsLoaded);
-        const onlyFields = this.headersMapping();
-        exportCsv(recordsToCSV, {
-          onlyFields,
+        exportCsv(recordsLoaded, {
           excludeFields: ['id'],
+          explicitlyIncludeFields: ['proxy.firstName', 'proxy.lastName', 'proxy.barcode']
         });
         this.csvExportPending = false;
       }
     }
-  }
-
-  headersMapping() {
-    const headers = ['requestType', 'status', 'requestExpirationDate', 'holdShelfExpirationDate',
-      'position', 'item.barcode', 'item.title', 'item.contributorNames', 'item.shelfLocation',
-      'item.callNumber', 'item.enumeration', 'item.status', 'loan.dueDate', 'requester.firstName',
-      'requester.barcode', 'requester.patronGroup', 'fulfilmentPreference', 'requester.pickupServicePoint',
-      'requester.deliveryAddress', 'proxy.firstName', 'proxy.barcode'];
-
-    const headersArray = headers.map(item => {
-      return {
-        label: this.props.intl.formatMessage({ id: `ui-requests.${item}` }),
-        value: item
-      };
-    });
-    return headersArray;
-  }
-
-  buildRecords(recordsLoaded) {
-    recordsLoaded.forEach(record => {
-      const contributorNamesMap = [];
-      if (record.item.contributorNames.length > 0) {
-        record.item.contributorNames.forEach(item => {
-          contributorNamesMap.push(item.name);
-        });
-      }
-      record.item.contributorNames = contributorNamesMap.join('; ');
-    });
-    return recordsLoaded;
   }
 
   // idType can be 'id', 'barcode', etc.
