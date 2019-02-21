3c7d0c164cb4b1a9756c84428392c5010f5527f1
Michal Kuklis <michal.kuklis@gmail.com>
2019-02-21 14:10:20 -0500

    Cleanup

--- src/RequestForm.js
+++ src/RequestForm.js
@@ -415,10 +415,9 @@ class RequestForm extends React.Component {
       unset(data, 'requestExpirationDate');
     }
 
-    if (holdShelfExpirationDate) {
-      const date = holdShelfExpirationDate.split('T')[0];
+    if (holdShelfExpirationDate && !holdShelfExpirationDate.match('T')) {
       const time = moment.tz(timeZone).format('HH:mm:ss');
-      data.holdShelfExpirationDate = moment.tz(`${date} ${time}`, timeZone).utc().format();
+      data.holdShelfExpirationDate = moment.tz(`${holdShelfExpirationDate} ${time}`, timeZone).utc().format();
     } else {
       unset(data, 'holdShelfExpirationDate');
     }
