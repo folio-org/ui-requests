a3b230a9760d1e86095eec817fcf4e092ff56636
nielserik <nielserik@indexdata.com>
2019-03-13 13:02:54 +0100

    Uireq 235 allow requests on in transit awaiting pickup (#310)
    * Remove async validation of request type against item status UIREQ-235

--- src/asyncValidate.js
+++ src/asyncValidate.js
@@ -1,19 +1,10 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 
-import { requestStatuses } from './constants';
-
-function getItemErrors(item, values) {
+function getItemErrors(item) {
   let error = null;
-
   if (!item) {
     error = { item: { barcode: <FormattedMessage id="ui-requests.errors.itemBarcodeDoesNotExist" /> } };
-  } else if (item.status.name !== requestStatuses.CHECKED_OUT) {
-    if (values.requestType === requestStatuses.RECALL) {
-      error = { item: { barcode: <FormattedMessage id="ui-requests.errors.onlyCheckedOutForRecall" /> } };
-    } else if (values.requestType === requestStatuses.HOLD) {
-      error = { item: { barcode: <FormattedMessage id="ui-requests.errors.onlyCheckedOutForHold" /> } };
-    }
   }
 
   return error;
@@ -25,7 +16,7 @@ function asyncValidateItem(values, props) {
     const query = `(barcode="${values.item.barcode}")`;
     uv.reset();
     uv.GET({ params: { query } }).then((items) => {
-      const errors = getItemErrors(items[0], values);
+      const errors = getItemErrors(items[0]);
       if (errors) {
         reject(errors);
       } else {
