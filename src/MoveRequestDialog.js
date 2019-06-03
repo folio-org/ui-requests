a52d13c9171aff8f49981c1c2f87c370f689b37d
Michal Kuklis <michal.kuklis@gmail.com>
2019-06-03 14:08:50 -0400

    Continue on moving request from one item to another

--- src/MoveRequestDialog.js
+++ src/MoveRequestDialog.js
@@ -1,12 +1,51 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { countBy } from 'lodash';
+import { countBy, get, orderBy } from 'lodash';
 import { FormattedMessage } from 'react-intl';
 import {
   Modal,
   ModalFooter,
   Button,
+  MultiColumnList,
+  Pane,
+  Icon,
 } from '@folio/stripes/components';
+
+import css from './MoveRequestDialog.css';
+
+const COLUMN_NAMES = [
+  'barcode',
+  'itemStatus',
+  'requestQueue',
+  'location',
+  'materialType',
+  'loanType',
+];
+
+const COLUMN_WIDTHS = {
+  barcode: '16%',
+  itemStatus: '16%',
+  requestQueue: '16%',
+  location: '16%',
+  materialType: '16%',
+  loanType: '16%',
+};
+
+const COLUMN_MAP = {
+  barcode: <FormattedMessage id="ui-requests.barcode" />,
+  itemStatus: <FormattedMessage id="ui-requests.item.status" />,
+  requestQueue: <FormattedMessage id="ui-requests.requestQueue" />,
+  location: <FormattedMessage id="ui-requests.location" />,
+  materialType: <FormattedMessage id="ui-requests.materialType" />,
+  loanType: <FormattedMessage id="ui-requests.loanType" />,
+};
+
+const formatter = {
+  itemStatus: item => item.status.name,
+  location: item => get(item, 'effectiveLocation.name', ''),
+  materialType: item => item.materialType.name,
+  loanType: item => (item.temporaryLoanType ? get(item, 'temporaryLoanType.name', '') : get(item, 'permanentLoanType.name', '')),
+};
 
 class MoveRequestDialog extends React.Component {
   static manifest = {
@@ -68,44 +107,60 @@ class MoveRequestDialog extends React.Component {
     super(props);
     this.state = {
       items: [],
+      isLoading: true,
     };
   }
 
   async componentDidMount() {
-    const {
-      mutator,
-      request: { item: { instanceId } }
-    } = this.props;
-
-    mutator.holdings.reset();
-    mutator.items.reset();
-    mutator.requests.reset();
-
-    let query = `instanceId==${instanceId}`;
-    const holdings = await mutator.holdings.GET({ params: { query } });
-
-    query = holdings.map(h => `holdingsRecordId==${h.id}`).join(' or ');
-    const items = await mutator.items.GET({ params: { query } });
-
-    query = items.map(i => `itemId==${i.id}`).join(' or ');
-    query = `(${query}) and (status="Open")`;
-    const requests = await mutator.requests.GET({ params: { query } });
+    const holdings = await this.fetchHoldings();
+    const items = await this.fetchItems(holdings);
+    const requests = await this.fetchRequests(items);
     const requestMap = countBy(requests, 'itemId');
 
     items.forEach(item => {
-      item.requestCount = requestMap[item.id] || 0;
+      item.requestQueue = requestMap[item.id] || 0;
     });
 
-    this.setState({ items });
+    this.setState({ items, isLoading: false });
+  }
+
+  async fetchHoldings() {
+    const {
+      mutator:  { holdings },
+      request: { item: { instanceId } }
+    } = this.props;
+    const query = `instanceId==${instanceId}`;
+    holdings.reset();
+
+    return holdings.GET({ params: { query } });
+  }
+
+  async fetchItems(holdings) {
+    const { mutator: { items } } = this.props;
+    const query = holdings.map(h => `holdingsRecordId==${h.id}`).join(' or ');
+    items.reset();
+
+    return items.GET({ params: { query } });
+  }
+
+  async fetchRequests(items) {
+    const { mutator: { requests } } = this.props;
+    let query = items.map(i => `itemId==${i.id}`).join(' or ');
+    query = `(${query}) and (status="Open")`;
+    requests.reset();
+
+    return requests.GET({ params: { query } });
+  }
+
+  onItemSelected = (item) => {
+
   }
 
   render() {
-    const {
-      open,
-      onClose,
-    } = this.props;
-
-    const { items } = this.state;
+    const { open, onClose } = this.props;
+    const { items, isLoading } = this.state;
+    const contentData = orderBy(items, 'requestQueue');
+    const count = items.length;
 
     const footer = (
       <ModalFooter>
@@ -114,7 +169,7 @@ class MoveRequestDialog extends React.Component {
           buttonStyle="primary"
           onClick={onClose}
         >
-          <FormattedMessage id="stripes-core.button.back" />
+          <FormattedMessage id="stripes-core.button.close" />
         </Button>
       </ModalFooter>
     );
@@ -122,13 +177,35 @@ class MoveRequestDialog extends React.Component {
     return (
       <Modal
         data-test-move-request-modal
-        label={<FormattedMessage id="ui-requests.move.selectItem" />}
+        label={<FormattedMessage id="ui-requests.moveRequest.selectItem" />}
         open={open}
+        contentClass={css.content}
         onClose={onClose}
         footer={footer}
         dismissible
       >
-        <p />
+        <Pane
+          padContent={false}
+          paneTitle={<FormattedMessage id="ui-requests.moveRequest.instanceItems" />}
+          paneSub={<FormattedMessage id="ui-requests.resultCount" values={{ count }} />}
+          noOverflow
+        >
+          {isLoading
+            ? <Icon icon="spinner-ellipsis" />
+            : <MultiColumnList
+              id="instance-items-list"
+              interactive
+              ariaLabel={<FormattedMessage id="ui-requests.moveRequest.instanceItems" />}
+              contentData={contentData}
+              visibleColumns={COLUMN_NAMES}
+              columnMapping={COLUMN_MAP}
+              columnWidths={COLUMN_WIDTHS}
+              formatter={formatter}
+              isEmptyMessage={<FormattedMessage id="ui-requests.moveRequest.instanceItems.notFound" />}
+              onRowClick={(_, item) => this.onItemSelected(item)}
+            />
+          }
+        </Pane>
       </Modal>
     );
   }
