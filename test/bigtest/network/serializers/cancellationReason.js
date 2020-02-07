cb31dcd1c3d5c8ed52faefb947ec4ef1fc9a1211
Anna_Melnyk <Anna_Melnyk@epam.com>
2020-02-07 12:58:57 +0200

    UIREQ-407: Spread json

--- test/bigtest/network/serializers/cancellationReason.js
+++ test/bigtest/network/serializers/cancellationReason.js
@@ -5,6 +5,6 @@ export default ApplicationSerializer.extend({
     const json = ApplicationSerializer.prototype.serialize.apply(this, args);
     const totalRecords = json.cancellationReasons.length;
 
-    return { json, totalRecords };
+    return { ...json, totalRecords };
   }
 });
