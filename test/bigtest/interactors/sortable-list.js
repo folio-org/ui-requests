4f97ee7fdbf97d73f7aa5b37b37011eb2778720c
Zak Burke <zburke@ebsco.com>
2021-12-20 12:49:10 -0500

    UIREQ-698 upgrade to current react-beautiful-dnd (#829)
    Older versions of `react-beautiful-dnd` drag in an unsupported version
    of `core-js` that causes a build warning. Update to a current version
    and adjust the `sortable-list` interactor to reflect changes to the
    attributes on the list-wrapper element.
    
    Refs UIREQ-698, STRIPES-675

--- test/bigtest/interactors/sortable-list.js
+++ test/bigtest/interactors/sortable-list.js
@@ -9,8 +9,17 @@ import {
 import DraggableRow from './draggable-row';
 
 @interactor class SortableListInteractor {
-  log = text('[role="log"]');
-  logPresent = isPresent('[role="log"]');
+  // rbd v11 placed three attributes, role=log, aria-live=assertive, and aria-atomic
+  // on the container of the sortable list.
+  // `role=log` was removed in https://github.com/atlassian/react-beautiful-dnd/commit/45ef30cd3ea422f8b9fb7eb142072f7e97b5272b#diff-98d5d7eb2b2f94535a8212ec54f4ec17c94c1bf123d9ffd0f22d0ec687a60a33
+  // as part of https://github.com/atlassian/react-beautiful-dnd/pull/1741
+  // to clean up lift announcements (https://github.com/atlassian/react-beautiful-dnd/issues/1695)
+  //
+  // because log and logPresent are part of the public API here,
+  // the name has been left in place
+  log = text('[aria-live="assertive"][aria-atomic]');
+  logPresent = isPresent('[aria-live="assertive"][aria-atomic]');
+
   rows = collection('[data-test-draggable-row]', DraggableRow);
 
   row = scoped('#row-1', DraggableRow);
