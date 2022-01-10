c1266c2536194a328a8cc815b275f6330f686503
Stanislau Snapok <stanislau_snapok@epam.com>
2022-01-10 12:51:07 +0300

    UIREQ-714: User is not redirected to "Item page"

--- src/ItemDetail.js
+++ src/ItemDetail.js
@@ -12,18 +12,35 @@ import {
 
 import { openRequestStatusFilters } from './utils';
 
-const ItemDetail = ({ item, loan, requestCount }) => {
-  if (!item.id && !item.barcode) {
+const MISSING_VALUE_SYMBOL = '-';
+
+const ItemDetail = ({
+  currentInstanceId,
+  request,
+  item,
+  loan,
+  requestCount,
+}) => {
+  const itemId = request?.itemId || item.id;
+
+  if (!itemId && !item.barcode) {
     return <FormattedMessage id="ui-requests.actions.loading" />;
   }
 
-  const recordLink = item ? <Link to={`/inventory/view/${item.instanceId}/${item.holdingsRecordId}/${item.id}`}>{item.barcode || item.id}</Link> : '-';
-  const status = get(item, 'status.name') || get(item, 'status');
-  const contributor = get(item, ['contributorNames', '0', 'name'], '-');
+  const instanceId = request?.instanceId || currentInstanceId;
+  const holdingsRecordId = request?.holdingsRecordId || item.holdingsRecordId;
+  const title = request?.instance.title || item.title || MISSING_VALUE_SYMBOL;
+  const contributor = get(request, ['instance', 'contributorNames', '0', 'name']) || get(item, ['contributorNames', '0', 'name']) || MISSING_VALUE_SYMBOL;
+  const count = request?.requestCount || requestCount;
+  const status = get(item, 'status.name') || get(item, 'status') || MISSING_VALUE_SYMBOL;
+  const effectiveLocationName = get(item, 'effectiveLocation.name') || get(item, 'location.name') || MISSING_VALUE_SYMBOL;
+  const dueDate = loan?.dueDate ? <FormattedDate value={loan.dueDate} /> : MISSING_VALUE_SYMBOL;
 
+  const effectiveCallNumberString = effectiveCallNumber(item);
+  const recordLink = itemId ? <Link to={`/inventory/view/${instanceId}/${holdingsRecordId}/${itemId}`}>{item.barcode || itemId}</Link> : MISSING_VALUE_SYMBOL;
   const positionLink = (
-    <Link to={`/requests?filters=${openRequestStatusFilters}&query=${item.id}&sort=Request Date`}>
-      {requestCount}
+    <Link to={`/requests?filters=${openRequestStatusFilters}&query=${itemId}&sort=Request Date`}>
+      {count}
     </Link>
   );
   const itemLabel = item.barcode ? 'ui-requests.item.barcode' : 'ui-requests.item.id';
@@ -43,7 +60,7 @@ const ItemDetail = ({ item, loan, requestCount }) => {
         </Col>
         <Col xs={4}>
           <KeyValue label={<FormattedMessage id="ui-requests.item.title" />}>
-            {get(item, ['title'], '-')}
+            {title}
           </KeyValue>
         </Col>
         <Col xs={4}>
@@ -55,24 +72,24 @@ const ItemDetail = ({ item, loan, requestCount }) => {
       <Row>
         <Col xs={4}>
           <KeyValue label={<FormattedMessage id="ui-requests.item.effectiveLocation" />}>
-            {get(item, 'effectiveLocation.name') || get(item, 'location.name') || ''}
+            {effectiveLocationName}
           </KeyValue>
         </Col>
         <Col xs={8}>
           <KeyValue label={<FormattedMessage id="ui-requests.item.callNumber" />}>
-            {effectiveCallNumber(item)}
+            {effectiveCallNumberString}
           </KeyValue>
         </Col>
       </Row>
       <Row>
         <Col xs={4}>
           <KeyValue label={<FormattedMessage id="ui-requests.item.status" />}>
-            {status || '-'}
+            {status}
           </KeyValue>
         </Col>
         <Col xs={4}>
           <KeyValue label={<FormattedMessage id="ui-requests.item.dueDate" />}>
-            {loan && loan.dueDate ? <FormattedDate value={loan.dueDate} /> : '-'}
+            {dueDate}
           </KeyValue>
         </Col>
         <Col
@@ -89,7 +106,9 @@ const ItemDetail = ({ item, loan, requestCount }) => {
 };
 
 ItemDetail.propTypes = {
-  item: PropTypes.object,
+  currentInstanceId: PropTypes.string,
+  request: PropTypes.object,
+  item: PropTypes.object.isRequired,
   loan: PropTypes.object,
   requestCount: PropTypes.number,
 };
