654bfdba58f259f7f2618316f5a93f34d843d148
Artem_Blazhko <artem_blazhko@epam.com>
2022-05-19 12:02:48 +0300

    change messages for RequestForm

--- src/RequestForm.js
+++ src/RequestForm.js
@@ -1468,7 +1468,7 @@ class RequestForm extends React.Component {
                             {isEditForm &&
                             <KeyValue
                               label={<FormattedMessage id="ui-requests.requestType" />}
-                              value={request.requestType ? <FormattedMessage id={requestTypesTranslations[request.requestType]} /> : request.requestType}
+                              value={<FormattedMessage id={requestTypesTranslations[request.requestType]} />}
                             /> }
                             {requestTypeError &&
                             <KeyValue
@@ -1480,7 +1480,7 @@ class RequestForm extends React.Component {
                             {isEditForm &&
                             <KeyValue
                               label={<FormattedMessage id="ui-requests.status" />}
-                              value={request.status ? <FormattedMessage id={requestStatusesTranslations[request.status]} /> : request.status}
+                              value={<FormattedMessage id={requestStatusesTranslations[request.status]} />}
                             /> }
                           </Col>
                           <Col xs={2}>
