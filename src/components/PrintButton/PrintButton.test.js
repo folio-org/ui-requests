310378555bb552ad2804674df8ed9d6b640e8c01
Artem Blazhko <artem_blazhko@epam.com>
2023-05-05 17:43:44 +0300

    Cover PrintButton by RTL/Jest tests (#1035)

---
+++ src/components/PrintButton/PrintButton.test.js
@@ -0,0 +1,54 @@
+import {
+  render,
+  screen,
+} from '@testing-library/react';
+
+import '../../../test/jest/__mock__';
+
+import ReactToPrint from 'react-to-print';
+
+import PrintButton from './PrintButton';
+
+const testIds = {
+  testContent: 'testContent',
+};
+const props = {
+  children: 'Print it',
+  contentRef: {
+    current: <div data-testid={testIds.testContent} />,
+  },
+  onAfterPrint: jest.fn(),
+  onBeforePrint: jest.fn(),
+  onBeforeGetContent: jest.fn(),
+};
+
+describe('PrintButton', () => {
+  beforeEach(() => {
+    render(<PrintButton {...props} />);
+  });
+
+  it('should call "ReactToPrint" with correct props', () => {
+    const expectedProps = {
+      removeAfterPrint: true,
+      onAfterPrint: props.onAfterPrint,
+      onBeforePrint: props.onBeforePrint,
+      onBeforeGetContent: props.onBeforeGetContent,
+      content: expect.any(Function),
+      trigger: expect.any(Function),
+    };
+
+    expect(ReactToPrint).toHaveBeenCalledWith(expectedProps, {});
+  });
+
+  it('should render button text', () => {
+    const buttonText = screen.getByText(props.children);
+
+    expect(buttonText).toBeVisible();
+  });
+
+  it('should render content', () => {
+    const content = screen.getByTestId(testIds.testContent);
+
+    expect(content).toBeVisible();
+  });
+});
