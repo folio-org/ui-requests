54dc89f78fd531e42bc3e310f233792340bd7214
VSnehalatha <snehalathav@ebsco.com>
2023-05-19 16:17:28 +0530

    Individual tests for each parse in RequestsFiltersConfig.test.js file

--- src/components/RequestsFilters/RequestsFiltersConfig.test.js
+++ src/components/RequestsFilters/RequestsFiltersConfig.test.js
@@ -4,23 +4,48 @@ import filtersConfig from './RequestsFiltersConfig';
 describe('RequestsFiltersConfig', () => {
   it('should have a filter for requestType', () => {
     const requestTypeFilter = filtersConfig.find(f => f.name === 'requestType');
-    expect(requestTypeFilter.cql).toEqual('requestType');
+    const expectedResult = {
+      name: 'requestType',
+      cql: 'requestType',
+      values: [],
+      operator: '==',
+    };
+    expect(requestTypeFilter).toEqual(expectedResult);
   });
 
   it('should have a filter for requestStatus', () => {
     const requestStatusFilter = filtersConfig.find(f => f.name === 'requestStatus');
-    expect(requestStatusFilter.cql).toEqual('status');
-    expect(requestStatusFilter.label).toEqual('ui-requests.requestMeta.status');
+    const expectedResult = {
+      name: 'requestStatus',
+      cql: 'status',
+      values: [],
+      operator: '==',
+      label: 'ui-requests.requestMeta.status'
+    };
+    expect(requestStatusFilter).toEqual(expectedResult);
   });
 
   it('should have a filter for request levels', () => {
     const requestLevelsFilter = filtersConfig.find(f => f.name === requestFilterTypes.REQUEST_LEVELS);
-    expect(requestLevelsFilter.cql).toEqual('requestLevel');
+    const expectedResult = {
+      name: 'requestLevels',
+      cql: 'requestLevel',
+      values: [],
+      operator: '==',
+    };
+    expect(requestLevelsFilter).toEqual(expectedResult);
   });
 
   it('should return the expected query string for a single tag', () => {
     const tagsFilter = filtersConfig.find(f => f.name === 'tags');
-    expect(tagsFilter.cql).toEqual('tags.tagList');
+    const expectedResult = {
+      name: 'tags',
+      cql: 'tags.tagList',
+      values: [],
+      operator: '==',
+      parse: expect.any(Function),
+    };
+    expect(tagsFilter).toEqual(expectedResult);
     expect(tagsFilter.parse('tag1')).toEqual('tags.tagList==*"*tag1*"*');
   });
   it('should return the expected query string for an array of tags', () => {
@@ -29,6 +54,12 @@ describe('RequestsFiltersConfig', () => {
   });
   it('should have a filter for pickup service point', () => {
     const pickupServicePointFilter = filtersConfig.find(f => f.name === requestFilterTypes.PICKUP_SERVICE_POINT);
-    expect(pickupServicePointFilter.cql).toEqual('pickupServicePointId');
+    const expectedResult = {
+      name: 'pickupServicePoints',
+      cql: 'pickupServicePointId',
+      values: [],
+      operator: '==',
+    };
+    expect(pickupServicePointFilter).toEqual(expectedResult);
   });
 });
