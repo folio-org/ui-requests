b406c57d27525ba47d512dc14d5320a9132a2970
ssandupatla <58558770+ssandupatla@users.noreply.github.com>
2023-06-27 12:45:12 +0530

    UIREQ-940 create JEST/RTL test cases for DraggableRow.js

--- src/components/SortableList/DraggableRow.test.js
+++ src/components/SortableList/DraggableRow.test.js
@@ -3,71 +3,57 @@ import { render, screen } from '@testing-library/react';
 import DraggableRow from './DraggableRow';
 
 describe('DraggableRow', () => {
-  const cells = [<div key="cell-1">Cell 1</div>, <div key="cell-2">Cell 2</div>];
+  const cells = [<div key="cell-1">Cell 1</div>];
   const rowProps = {
     additionalClasses: ['extra-class'],
   };
   const provided = {
     innerRef: jest.fn(),
-    draggableProps: {
-      style: { transform: 'translate(0, 0)' },
-      'aria-describedby': 'row-1',
-    },
+    draggableProps: {},
     dragHandleProps: {},
   };
-  test('renders DraggableRow correctly', () => {
+  const props = {
+    cells,
+    provided,
+    snapshot: {
+      isDragging: false,
+    },
+    rowClass: 'row-class',
+    rowIndex: 1,
+    rowProps,
+  };
+  test('should render DraggableRow', () => {
     render(
-      <DraggableRow
-        cells={cells}
-        provided={provided}
-        snapshot={{ isDragging: false }}
-        rowClass="row-class"
-        rowIndex={1}
-        rowProps={rowProps}
-      />
+      <DraggableRow {...props} />
     );
     expect(screen.getByText('Cell 1')).toBeInTheDocument();
   });
 
-  test('If isDragging prop false, class should be extra-class row-class', () => {
+  test('should have css "class extra-class row-class" if isDragging prop false', () => {
     render(
-      <DraggableRow
-        cells={cells}
-        provided={provided}
-        snapshot={{ isDragging: false }}
-        rowClass="row-class"
-        rowIndex={1}
-        rowProps={rowProps}
-      />
+      <DraggableRow {...props} />
     );
     expect(document.querySelector('[class="extra-class row-class"]')).toBeInTheDocument();
   });
 
-  test('renders DraggableRow using portal', () => {
+  const draggableProps = {
+    ...props,
+    snapshot: {
+      isDragging: true,
+    },
+  };
+
+  test('DraggableRow using portal', () => {
     document.body.innerHTML = '<div id="ModuleContainer"></div>';
     render(
-      <DraggableRow
-        cells={cells}
-        provided={provided}
-        snapshot={{ isDragging: true }}
-        rowClass="row-class"
-        rowIndex={1}
-        rowProps={rowProps}
-      />
+      <DraggableRow {...draggableProps} />
     );
     expect(document.getElementById('ModuleContainer')).toBeInTheDocument();
   });
 
-  test('If isDragging prop True, class should be extra-class row-class DraggableRow', () => {
+  test('should have css "class extra-class row-class DraggableRow" if isDragging prop true', () => {
     render(
-      <DraggableRow
-        cells={cells}
-        provided={provided}
-        snapshot={{ isDragging: true }}
-        rowClass="row-class"
-        rowIndex={1}
-        rowProps={rowProps}
-      />
+      <DraggableRow {...draggableProps} />
     );
     expect(document.querySelector('[class="extra-class row-class DraggableRow"]')).toBeInTheDocument();
   });
