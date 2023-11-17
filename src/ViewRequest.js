5db080c03c0cd3712c76e714dd47da6d7b8901b2
Priyanka Terala <104053200+Terala-Priyanka@users.noreply.github.com>
2023-11-17 15:00:31 +0530

    UIREQ-1040 - Hide Actions menu on closed request of DCB Transaction (#1123)

--- src/ViewRequest.js
+++ src/ViewRequest.js
@@ -513,32 +513,28 @@ class ViewRequest extends React.Component {
     const showActionMenu = stripes.hasPerm('ui-requests.create')
       || stripes.hasPerm('ui-requests.edit')
       || stripes.hasPerm('ui-requests.moveRequest')
-      || stripes.hasPerm('ui-requests.reorderQueue');
+      || stripes.hasPerm('ui-requests.reorderQueue') || !isDCBTransaction;
 
     const actionMenu = ({ onToggle }) => {
       if (isRequestClosed) {
-        if (!isRequestValid || (requestLevel === REQUEST_LEVEL_TYPES.TITLE && !titleLevelRequestsFeatureEnabled)) {
+        if (!isRequestValid || (requestLevel === REQUEST_LEVEL_TYPES.TITLE && !titleLevelRequestsFeatureEnabled) || isDCBTransaction) {
           return null;
         }
 
         return (
           <IfPermission perm="ui-requests.create">
-            {
-              !isDCBTransaction && (
-                <Button
-                  id="duplicate-request"
-                  onClick={() => {
-                    onToggle();
-                    this.props.onDuplicate(request);
-                  }}
-                  buttonStyle="dropdownItem"
-                >
-                  <Icon icon="duplicate">
-                    <FormattedMessage id="ui-requests.actions.duplicateRequest" />
-                  </Icon>
-                </Button>
-              )
-            }
+            <Button
+              id="duplicate-request"
+              onClick={() => {
+                onToggle();
+                this.props.onDuplicate(request);
+              }}
+              buttonStyle="dropdownItem"
+            >
+              <Icon icon="duplicate">
+                <FormattedMessage id="ui-requests.actions.duplicateRequest" />
+              </Icon>
+            </Button>
           </IfPermission>
         );
       }
