858b2eeeb630879cefef5ab9e108339504495619
Dmitriy-Litvinenko <Dmitriy_Litvinenko@epam.com>
2024-11-12 18:35:46 +0200

    UIREQ-1180: Fix DOMPurify import

--- src/routes/RequestsRoute.js
+++ src/routes/RequestsRoute.js
@@ -15,7 +15,7 @@ import {
   FormattedMessage,
   injectIntl,
 } from 'react-intl';
-import { sanitize } from 'dompurify';
+import DOMPurify from 'dompurify';
 
 import {
   AppIcon,
@@ -1226,7 +1226,7 @@ class RequestsRoute extends React.Component {
     const slipTypeInLowerCase = slipType.toLowerCase();
     const slipTemplate = staffSlips.find(slip => slip.name.toLowerCase() === slipTypeInLowerCase);
 
-    return sanitize(get(slipTemplate, 'template', ''), { ADD_TAGS: ['Barcode'] });
+    return DOMPurify.sanitize(get(slipTemplate, 'template', ''), { ADD_TAGS: ['Barcode'] });
   }
 
   handleFilterChange = ({ name, values }) => {
