8f5ae52d79513d9c86189e5fe2818b3972540f0a
Mike Taylor <mike@indexdata.com>
2025-08-11 11:49:29 +0100

    Remove <FullItemDetail> code

--- src/deprecated/components/ViewRequest/ViewRequest.js
+++ src/deprecated/components/ViewRequest/ViewRequest.js
@@ -43,7 +43,7 @@ import {
 
 import ViewRequestShortcutsWrapper from '../../../components/ViewRequestShortcutsWrapper';
 import CancelRequestDialog from '../../../CancelRequestDialog';
-import FullItemDetail from './FullItemDetail';
+import ItemDetail from '../../../ItemDetail';
 import TitleInformation from '../../../components/TitleInformation';
 import UserDetail from '../../../UserDetail';
 import RequestFormContainer from '../RequestFormContainer/RequestFormContainer';
@@ -726,7 +726,7 @@ class ViewRequest extends React.Component {
               >
                 { item
                   ? (
-                    <FullItemDetail
+                    <ItemDetail
                       request={request}
                       item={request.item}
                       loan={request.loan}
