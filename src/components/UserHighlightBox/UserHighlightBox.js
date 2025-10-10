7f85f12e380e9859d937dddd920e949a3ec8158c
Katherine Bargar <kbargar@fivecolleges.edu>
2025-10-10 16:45:32 +0000

    Replace the internals in UserHighlightBox with new components

--- src/components/UserHighlightBox/UserHighlightBox.js
+++ src/components/UserHighlightBox/UserHighlightBox.js
@@ -1,30 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 import { FormattedMessage } from 'react-intl';
 
 import {
   Col,
   Headline,
   Row,
-  NoValue,
 } from '@folio/stripes/components';
 
 import css from '../../requests.css';
-import { getFullName } from '../../utils';
-
-const createUserHighlightBoxLink = (linkText, id) => {
-  return linkText ? <Link to={`/users/view/${id}`}>{linkText}</Link> : '';
-};
+import { BarcodeLink, FullNameLink } from '../RequesterLinks';
 
 const UserHighlightBox = ({
   title,
   user,
 }) => {
-  const name = user ? getFullName(user) : null;
-  const recordLink = createUserHighlightBoxLink(name, user?.id);
-  const barcodeLink = createUserHighlightBoxLink(user?.barcode, user?.id);
-
   return (
     <Row>
       <Col xs={12}>
@@ -33,11 +23,11 @@ const UserHighlightBox = ({
             {title}
           </Headline>
           <div>
-            {recordLink || <FormattedMessage id="ui-requests.errors.user.unknown" />}
+            <FullNameLink request={{ requester:user }} />
             {' '}
             <FormattedMessage id="ui-requests.barcode" />:
             {' '}
-            {user?.barcode ? barcodeLink : <NoValue />}
+            <BarcodeLink request={{ requester:user }} />
           </div>
         </div>
       </Col>
