c149758f5c5b713cbe7d0b8727b04cd859e12ed2
Katherine Bargar <kbargar@fivecolleges.edu>
2025-10-22 14:32:53 +0000

    Properly render unknown/anonymized for requester

--- src/components/RequesterLinks/RequesterLinks.test.js
+++ src/components/RequesterLinks/RequesterLinks.test.js
@@ -78,25 +78,25 @@ describe('BarcodeLink', () => {
 });
 
 describe('FullNameLink', () => {
+  let mockedRequest;
   let getFullNameSpy;
   beforeEach(() => {
+    mockedRequest = {
+      requesterId: 'testRequestRequesterId',
+    };
     getFullNameSpy = jest.spyOn(utils, 'getFullName');
   });
   afterEach(() => {
     getFullNameSpy.mockClear();
   });
-  describe('When name is presented', () => {
+  describe('When requester is present', () => {
     let mockedRequester;
-    let mockedRequest;
     beforeEach(() => {
       mockedRequester = {
         id: 'testRequesterId',
         lastName: 'lastName',
       };
-      mockedRequest = {
-        requesterId: 'testRequestRequesterId',
-        requester: mockedRequester,
-      };
+      mockedRequest.requester = mockedRequester;
     });
 
     it('should render `Link` with correct label', () => {
@@ -138,18 +138,30 @@ describe('FullNameLink', () => {
     });
   });
 
-  describe('When name is not presented', () => {
-    beforeEach(() => {
-      render(
-        <MemoryRouter>
-          <FullNameLink request={{}} />
-        </MemoryRouter>
-      );
+  describe('When requester is not presented', () => {
+    describe('and the requesterId exists', () => {
+      it('should render unknown', () => {
+        render(
+          <MemoryRouter>
+            <FullNameLink request={mockedRequest} />
+          </MemoryRouter>
+        );
+        expect(screen.queryAllByRole('link')).toHaveLength(0);
+        expect(screen.queryAllByText('ui-requests.requestMeta.unknown')).toHaveLength(1);
+      });
     });
 
-    it('should render anonymized', () => {
-      expect(screen.queryAllByRole('link')).toHaveLength(0);
-      expect(screen.queryAllByText('ui-requests.requestMeta.anonymized')).toHaveLength(1);
+    describe('and the requesterId does not exist', () => {
+      it('should render anonymized', () => {
+        mockedRequest.requesterId = null;
+        render(
+          <MemoryRouter>
+            <FullNameLink request={mockedRequest} />
+          </MemoryRouter>
+        );
+        expect(screen.queryAllByRole('link')).toHaveLength(0);
+        expect(screen.queryAllByText('ui-requests.requestMeta.anonymized')).toHaveLength(1);
+      });
     });
   });
 });
