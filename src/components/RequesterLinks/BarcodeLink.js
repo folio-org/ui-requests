a347e8d3002cd44afcce5c1da5abc3ab2327e9f3
Katherine Bargar <brycekbargar@gmail.com>
2025-11-13 05:01:04 -0500

    Fixes for UIREQ-1313 code review
    * Split RequesterLinks tests into two files
    * Replace let with const in test code
    * Remove unneeded react imports
    * Remove explicitly checking for null in FullNameLink

--- src/components/RequesterLinks/BarcodeLink.js
+++ src/components/RequesterLinks/BarcodeLink.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 
 import { NoValue } from '@folio/stripes/components';
--- src/components/RequesterLinks/FullNameLink.js
+++ src/components/RequesterLinks/FullNameLink.js
@@ -1,19 +1,23 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import { FormattedMessage } from 'react-intl';
 
 import { getFullName } from '../../utils';
 import propTypes from './propTypes';
 
+// This link component takes both the userId and the user object
+// If a requester or proxy has been deleted the request will still have the requesterId or proxyUserId
+// but not have the actual requester/proxy object
+// If a requester has been anonymized then the requesterId and proxyUserId will be null
+// in addition to the requester/proxy object
 const FullNameLink = ({
   userId,
   user,
 }) => {
-  if (user != null) {
+  if (user) {
     return <Link to={`/users/view/${user?.id ?? userId}`}>{getFullName(user)}</Link>;
   }
 
-  return userId != null
+  return userId
     ? <FormattedMessage id="ui-requests.errors.user.unknown" />
     : <FormattedMessage id="ui-requests.requestMeta.anonymized" />;
 };
