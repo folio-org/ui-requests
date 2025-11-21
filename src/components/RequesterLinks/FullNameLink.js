7d28677fd1d45bd4f76d74929781e79989fe57b0
Katherine Bargar <kbargar@fivecolleges.edu>
2025-11-21 14:29:33 +0000

    Use jsdoc format for fullnamelink comment

--- src/components/RequesterLinks/FullNameLink.js
+++ src/components/RequesterLinks/FullNameLink.js
@@ -4,11 +4,16 @@ import { FormattedMessage } from 'react-intl';
 import { getFullName, isUserAnonymized } from '../../utils';
 import propTypes from './propTypes';
 
-// This link component takes both the userId and the user object
-// If a requester or proxy has been deleted the request will still have the requesterId or proxyUserId
-// but not have the actual requester/proxy object
-// If a requester has been anonymized then the requesterId and proxyUserId will be null
-// in addition to the requester/proxy object
+/**
+FullNameLink component displays a link to the user's profile using their userId and user object.
+If the user object is present, it renders a link with the user's full name.
+If the user is anonymized (userId === null), it displays an anonymized message.
+If the user is not found, it displays an unknown user message.
+@param {Object} props
+@param {string|null|undefined} props.userId - The user's identifier.
+@param {Object|null|undefined} props.user - The user object.
+@returns {JSX.Element} A link to the user profile or a message.
+*/
 const FullNameLink = ({
   userId,
   user,
